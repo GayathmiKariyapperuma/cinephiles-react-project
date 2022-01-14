834b31102d73b56695f20ff53d00a6eaa2137ac3
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-15 01:03:12 +0530

    initialize firebase

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 import App from "./App";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBUmgGI0la1SlgEZY-bE3TiRvotq-1bNXo",
+  authDomain: "cinephiles-react-project.firebaseapp.com",
+  projectId: "cinephiles-react-project",
+  storageBucket: "cinephiles-react-project.appspot.com",
+  messagingSenderId: "538162429481",
+  appId: "1:538162429481:web:4e2e2e70ec5356b624e638",
+  measurementId: "G-ZBSNKKH92X"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
