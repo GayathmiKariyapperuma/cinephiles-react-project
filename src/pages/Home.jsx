42c8ff1ab474655b552114def0485398a537f730
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-19 17:31:13 +0530

    edit home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 function Home() {
-  const [toggle, setToggle] = useState(false);
-
   return (
-    <>
-      <h1>Getting Started with React</h1>
-      {toggle && (
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-          voluptatibus eligendi assumenda culpa odio maxime veritatis aperiam.
-          Cupiditate, enim, provident eaque officiis iusto eligendi, modi unde
-          voluptatum officia asperiores incidunt.
-        </p>
-      )}
-      <button onClick={() => setToggle(!toggle)}>add 1</button>
-    </>
+    <div>
+      <h1>Getting Started with React</h1>{" "}
+    </div>
   );
 }
 
