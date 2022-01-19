2edeb71f8077ad15774a9fdb2c05111d1f4f56e5
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-19 15:14:47 +0530

    add css

--- src/pages/Top50.jsx
+++ src/pages/Top50.jsx
@@ -4,15 +4,51 @@ function Top50() {
   return (
     <div>
       <h1>IMDb Top 50 movies</h1>
-      <div class="topContainer">
-        <img
-          src="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg"
-          alt="Avatar"
-          class="image"
-        />
-        <div class="overlay">
-          <div class="text">
-            <button class="button">View Details</button>
+      <div className="topRow">
+        <div className="topColumn">
+          <div class="topContainer">
+            <img
+              src="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg"
+              alt="Avatar"
+              class="image"
+            />
+            <div class="overlay">
+              <div class="text">
+                <button class="button">View Details</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="topRow">
+        <div className="topColumn">
+          <div class="topContainer">
+            <img
+              src="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg"
+              alt="Avatar"
+              class="image"
+            />
+            <div class="overlay">
+              <div class="text">
+                <button class="button">View Details</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="topRow">
+        <div className="topColumn">
+          <div class="topContainer">
+            <img
+              src="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg"
+              alt="Avatar"
+              class="image"
+            />
+            <div class="overlay">
+              <div class="text">
+                <button class="button">View Details</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
