63fcf4f32d1894eb3820c00dd5cc8e9e2b334583
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-19 23:27:16 +0530

    initiate tv series

--- src/pages/TVSeries.jsx
+++ src/pages/TVSeries.jsx
@@ -1,7 +1,222 @@
 import React from "react";
 
 function TVSeries() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Best Picture-Winning Movies of the past decade</h1>
+      <div className="topRow">
+        <div className="topColumn">
+          <div class="topContainer">
+            <img
+              src="https://lumiere-a.akamaihd.net/v1/images/pp_nomadland_20985_eb18bd46.jpeg"
+              alt="Movie Poster"
+              class="image"
+            />
+            <div class="overlay">
+              <div class="text">
+                <p>
+                  #1 <br />
+                  <br />
+                  Nomadland (2020) <br />
+                  <br /> IMDb 7.3
+                </p>
+                <button class="button">View Details</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="topColumn">
+          <div class="topContainer">
+            <img
+              src="https://images.squarespace-cdn.com/content/v1/5a4488a6ace864a5558b6738/1575653762434-4HIFKWKBP8W893A50I95/p16965677_v_v8_aa.jpg"
+              alt="Movie Poster"
+              class="image"
+            />
+            <div class="overlay">
+              <div class="text">
+                <p>
+                  #2 <br />
+                  <br />
+                  Parasite (2019) <br />
+                  <br /> IMDb 8.6
+                </p>
+                <button class="button">View Details</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="topColumn">
+          <div class="topContainer">
+            <img
+              src="https://movies.universalpictures.com/media/gbk-tsr1sheet-rgb-3sm-5b7308033d1a5-1.jpg"
+              alt="Movie Poster"
+              class="image"
+            />
+            <div class="overlay">
+              <div class="text">
+                <p>
+                  #3 <br />
+                  <br />
+                  Green Book (2018) <br />
+                  <br /> IMDb 8.2
+                </p>
+                <button class="button">View Details</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="topColumn">
+          <div class="topContainer">
+            <img
+              src="https://m.media-amazon.com/images/M/MV5BNGNiNWQ5M2MtNGI0OC00MDA2LWI5NzEtMmZiYjVjMDEyOWYzXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_.jpg"
+              alt="Movie Poster"
+              class="image"
+            />
+            <div class="overlay">
+              <div class="text">
+                <p>
+                  #4 <br />
+                  <br />
+                  The Shape of Water (2017) <br />
+                  <br /> IMDb 7.3
+                </p>
+                <button class="button">View Details</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="topColumn">
+          <div class="topContainer">
+            <img
+              src="https://m.media-amazon.com/images/M/MV5BNzQxNTIyODAxMV5BMl5BanBnXkFtZTgwNzQyMDA3OTE@._V1_.jpg"
+              alt="Movie Poster"
+              class="image"
+            />
+            <div class="overlay">
+              <div class="text">
+                <p>
+                  #5 <br />
+                  <br />
+                  Moonlight (2016) <br />
+                  <br /> IMDb 7.4
+                </p>
+                <button class="button">View Details</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="topColumn">
+          <div class="topContainer">
+            <img
+              src="https://m.media-amazon.com/images/I/81FQRdN+D5L._AC_SY679_.jpg"
+              alt="Movie Poster"
+              class="image"
+            />
+            <div class="overlay">
+              <div class="text">
+                <p>
+                  #6 <br />
+                  <br />
+                  Spotlight (2015) <br />
+                  <br /> IMDb 8.1
+                </p>
+                <button class="button">View Details</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="topColumn">
+          <div class="topContainer">
+            <img
+              src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5f8ca897-7ada-4a3a-aad6-6ec6b44b881b/dehonsd-bfcd6a51-34cd-4e9e-b7de-a4515bc48b8a.jpg/v1/fill/w_1920,h_2640,q_75,strp/birdman_poster__bw_g__by_farbschmidt_dehonsd-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjY0MCIsInBhdGgiOiJcL2ZcLzVmOGNhODk3LTdhZGEtNGEzYS1hYWQ2LTZlYzZiNDRiODgxYlwvZGVob25zZC1iZmNkNmE1MS0zNGNkLTRlOWUtYjdkZS1hNDUxNWJjNDhiOGEuanBnIiwid2lkdGgiOiI8PTE5MjAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.Mi_xaAIApNixoctLIEYncQSwT1-CBhfgaADdCAdxFzM"
+              alt="Movie Poster"
+              class="image"
+            />
+            <div class="overlay">
+              <div class="text">
+                <p>
+                  #7 <br />
+                  <br />
+                  Birdman or (The Unexpected Virtue of Ignorance) (2014) <br />
+                  <br /> IMDb 7.7
+                </p>
+                <button class="button">View Details</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="topColumn">
+          <div class="topContainer">
+            <img
+              src="https://m.media-amazon.com/images/M/MV5BMjExMTEzODkyN15BMl5BanBnXkFtZTcwNTU4NTc4OQ@@._V1_.jpg"
+              alt="Movie Poster"
+              class="image"
+            />
+            <div class="overlay">
+              <div class="text">
+                <p>
+                  #8 <br />
+                  <br />
+                  12 Years a Slave (2013) <br />
+                  <br /> IMDb 8.1
+                </p>
+                <button class="button">View Details</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="topColumn">
+          <div class="topContainer">
+            <img
+              src="https://movieposters2.com/images/1065114-b.jpg"
+              alt="Movie Poster"
+              class="image"
+            />
+            <div class="overlay">
+              <div class="text">
+                <p>
+                  #9 <br />
+                  <br />
+                  Argo (2012) <br />
+                  <br /> IMDb 7.7
+                </p>
+                <button class="button">View Details</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="topColumn">
+          <div class="topContainer">
+            <img
+              src="https://m.media-amazon.com/images/M/MV5BMDUyZWU5N2UtOWFlMy00MTI0LTk0ZDYtMzFhNjljODBhZDA5XkEyXkFqcGdeQXVyNzA4ODc3ODU@._V1_.jpg"
+              alt="Movie Poster"
+              class="image"
+            />
+            <div class="overlay">
+              <div class="text">
+                <p>
+                  #10 <br />
+                  <br />
+                  The Artist (2011) <br />
+                  <br /> IMDb 7.9
+                </p>
+                <button class="button">View Details</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TVSeries;
