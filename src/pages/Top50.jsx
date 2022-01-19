373cecbd45a51d463444046e6ead1e980fe8bb6e
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-19 15:18:14 +0530

    position the overlays

--- src/pages/Top50.jsx
+++ src/pages/Top50.jsx
@@ -19,8 +19,7 @@ function Top50() {
             </div>
           </div>
         </div>
-      </div>
-      <div className="topRow">
+
         <div className="topColumn">
           <div class="topContainer">
             <img
@@ -35,8 +34,37 @@ function Top50() {
             </div>
           </div>
         </div>
-      </div>
-      <div className="topRow">
+
+        <div className="topColumn">
+          <div class="topContainer">
+            <img
+              src="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg"
+              alt="Avatar"
+              class="image"
+            />
+            <div class="overlay">
+              <div class="text">
+                <button class="button">View Details</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="topColumn">
+          <div class="topContainer">
+            <img
+              src="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg"
+              alt="Avatar"
+              class="image"
+            />
+            <div class="overlay">
+              <div class="text">
+                <button class="button">View Details</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div className="topColumn">
           <div class="topContainer">
             <img
