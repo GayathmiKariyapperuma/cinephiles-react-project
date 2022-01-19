a3a0fe9cc8e61ecaf212a8d1b20efbcf186c9432
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-19 20:32:45 +0530

    update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
         <img
           src="https://s3-us-west-2.amazonaws.com/prd-rteditorial/wp-content/uploads/2018/03/13153742/RT_300EssentialMovies_700X250.jpg"
           alt="bg"
-          width="1300rem"
+          width="1200rem"
         />
         <br />
         <h1>Cinephiles - The World of Cinema</h1>
