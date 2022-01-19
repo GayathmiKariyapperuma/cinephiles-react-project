d60ae08c8ee3564d8afcb040b8b460b45a61ce3f
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-19 22:06:14 +0530

    add contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,9 +2,31 @@ import React from "react";
 
 function Contact() {
   return (
-    <>
-      <h1>Contact page</h1>
-    </>
+    <div>
+      <h1>Contact Us</h1>
+      We'd love to hear from you
+      <div className="ContactContainer">
+        <form>
+          <div class="info">
+            <input
+              class="fname"
+              type="text"
+              name="name"
+              placeholder="Full name"
+            />
+            <input type="text" name="name" placeholder="Email" />
+            <input type="text" name="name" placeholder="Phone number" />
+          </div>
+          <p>Message</p>
+          <div>
+            <textarea rows="4"></textarea>
+          </div>
+          <button type="submit" href="/">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
