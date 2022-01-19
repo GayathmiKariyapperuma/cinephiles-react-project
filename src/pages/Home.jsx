bd5ae27ec8ad7d79f0ee3f1071abd995d51d9fde
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-19 17:55:14 +0530

    add slideshow

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Slide } from "react-slideshow-image";
 
 function Home() {
+  const [autoplay, setAutoplay] = useState(true);
+
+  const style = {
+    textAlign: "center",
+    background: "teal",
+    padding: "50px 0",
+    fontSize: "18px"
+  };
+
   return (
     <div>
-      <h1>Getting Started with React</h1>{" "}
+      <div>
+        <Slide autoplay={autoplay}>
+          <div style={style}>First Slide</div>
+          <div style={style}>Second Slide</div>
+          <div style={style}>Third Slide</div>
+        </Slide>
+      </div>
+
+      <div className="autoplay-buttons">
+        Autplay is {autoplay ? "on" : "off"}
+      </div>
+      <div className="autoplay-buttons">
+        <button type="button" onClick={() => setAutoplay(false)}>
+          Pause
+        </button>
+        <button type="button" onClick={() => setAutoplay(true)}>
+          Play
+        </button>
+      </div>
     </div>
   );
 }
