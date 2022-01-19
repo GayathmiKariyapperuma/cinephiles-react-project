ce5c3c286d63ca831b5e41edd4ce311f9df7e5ea
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-19 15:05:27 +0530

    add overlay image

--- src/pages/Top50.jsx
+++ src/pages/Top50.jsx
@@ -2,9 +2,19 @@ import React from "react";
 
 function Top50() {
   return (
-    <>
+    <div>
       <h1>IMDb Top 50 movies</h1>
-    </>
+      <div class="topContainer">
+        <img
+          src="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg"
+          alt="Avatar"
+          class="image"
+        />
+        <div class="overlay">
+          <div class="text">Hello World</div>
+        </div>
+      </div>
+    </div>
   );
 }
 
